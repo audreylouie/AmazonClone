88521e30328eced548b9877856fead957d0756d4
audreylouie <al83037n@pace.edu>
2025-05-29 13:15:50 -0400

    Updated cart quantity in checkout and homepage

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,7 +1,8 @@
-import {cart, addToCart} from '../data/cart.js';
+import {cart, addToCart, calculateCartQuantity} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
+updateCartQuantity();
 let productsHTML = '';
 
 products.forEach((product)=>{
@@ -60,11 +61,7 @@ products.forEach((product)=>{
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 function updateCartQuantity(){
-  let cartQuantity = 0;
-    cart.forEach((cartItem)=>{
-      cartQuantity += cartItem.quantity;
-    });
-
+  const cartQuantity = calculateCartQuantity();
     document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
 }
 
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,8 @@
-import {cart, removeFromCart} from '../data/cart.js';
+import {cart, removeFromCart, calculateCartQuantity} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
+updateCartQuantity();
 let cartSummaryHTML = '';
 
 cart.forEach((cartItem)=>{
@@ -100,5 +101,11 @@ document.querySelectorAll('.js-delete-link').forEach((link)=>{
     removeFromCart(productId);
     const container = document.querySelector(`.js-cart-item-container-${productId}`)
     container.remove();
+    updateCartQuantity();
   });
 });
+
+function updateCartQuantity(){
+const cartQuantity = calculateCartQuantity();
+document.querySelector('.js-checkout-quantity').innerHTML = `${cartQuantity} items`;
+}
