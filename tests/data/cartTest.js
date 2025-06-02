1f65769bb1dbfa0a544efa61e563e52428476e2f
audreylouie <al83037n@pace.edu>
2025-06-02 18:57:03 -0400

    Added test suite for remove product from cart

--- tests/data/cartTest.js
+++ tests/data/cartTest.js
@@ -1,4 +1,4 @@
-import { addToCart, cart, loadFromStorage } from "../../data/cart.js";
+import { addToCart, cart, loadFromStorage, removeFromCart } from "../../data/cart.js";
 
 describe('test suite: add to cart', ()=>{
   beforeEach(()=>{
@@ -39,6 +39,48 @@ describe('test suite: add to cart', ()=>{
     expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([{ productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 1,
     deliveryOptionId: '1'
-    }]))
+    }]));
   });
+});
+
+describe('test suite: remove from cart', () =>{
+  beforeEach(() => {
+    spyOn(localStorage, 'setItem');
+    spyOn(localStorage, 'getItem').and.callFake(()=>{
+      return JSON.stringify([{
+        productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 1,
+        deliveryOptionId: '1'
+      },{
+        productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 2,
+        deliveryOptionId: '2'
+      }]);
+    });
+    loadFromStorage();
+  });
+  it('removes a productId that is in the cart', () => {
+    removeFromCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    expect(cart.length).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([{
+      productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 2,
+      deliveryOptionId: '2'
+    }]));
+  });
+  it('removes a productId that is not in the cart', () => {
+      removeFromCart('83d4ca15-0f35-48f5-b7a3-1ea210004f2e');
+      expect(cart.length).toEqual(2);
+      expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+      expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([{
+        productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 1,
+        deliveryOptionId: '1'
+      },{
+        productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 2,
+        deliveryOptionId: '2'
+      }]));
+    });
 });
