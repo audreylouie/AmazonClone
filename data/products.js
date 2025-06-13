2de298d8b4e80985b65609f4f8114cba6ff87dbb
audreylouie <al83037n@pace.edu>
2025-06-13 17:56:17 -0400

    Made search bar interactive

--- data/products.js
+++ data/products.js
@@ -15,6 +15,7 @@ export class Product {
   name;
   rating;
   priceCents;
+  keywords;
 
   constructor(productDetails) {
     this.id = productDetails.id;
@@ -22,6 +23,7 @@ export class Product {
     this.name = productDetails.name;
     this.rating = productDetails.rating;
     this.priceCents = productDetails.priceCents;
+    this.keywords = productDetails.keywords;
   }
 
   getStarsUrl(){
